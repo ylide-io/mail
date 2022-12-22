35fb82350330d0ebc01cfce0e106043ee5f4366d
Oleg Cherr <olegcherr@gmail.com>
2022-12-22 14:18:55 +0100

    Adjust mailbox no-messages content

--- src/pages/MailboxPage/components/MailboxEmpty.tsx
+++ src/pages/MailboxPage/components/MailboxEmpty.tsx
@@ -1,8 +1,13 @@
 import { observer } from 'mobx-react';
 import ClickToCopy from '../../../controls/ClickToCopy';
 import domain from '../../../stores/Domain';
+import mailList from '../../../stores/MailList';
+import LinkButton from '../../../components/Sidebar/LinkButton';
+import React from 'react';
 
 const MailboxEmpty = observer(() => {
+	const activeFolderId = mailList.activeFolderId;
+
 	return (
 		<div
 			style={{
@@ -13,17 +18,26 @@ const MailboxEmpty = observer(() => {
 				padding: '100px 20px 150px',
 			}}
 		>
-			<h3>Your mailbox is empty yet.</h3>
-			<div style={{ marginTop: 6 }}>
-				<span>Share your addresses: </span>
-				{domain.accounts.activeAccounts.map(acc => (
-					<div>
-						<ClickToCopy dataToCopy={acc.account.address}>{acc.account.address}</ClickToCopy>
-					</div>
-				))}
+			{activeFolderId === 'inbox' ? <>
+				<h3>Your mailbox is empty yet.</h3>
+				<div style={{ marginTop: 6 }}>
+					<span>Share your addresses: </span>
+					{domain.accounts.activeAccounts.map(acc => (
+						<div>
+							<ClickToCopy
+								dataToCopy={acc.account.address}>{acc.account.address}</ClickToCopy>
+						</div>
+					))}
 
-				<span> with your friends to receive the first one!</span>
-			</div>
+					<span> with your friends to receive the first message.</span>
+				</div>
+			</> : activeFolderId === 'sent' ? <>
+				<h3 style={{ marginBottom: 16 }}>
+					You haven't sent any message yet.<br/>Send a message to your friend now.
+				</h3>
+
+				<LinkButton text='Compose Mail' link='/compose' />
+			</> : <h3>No messages here.</h3>}
 		</div>
 	);
 });
