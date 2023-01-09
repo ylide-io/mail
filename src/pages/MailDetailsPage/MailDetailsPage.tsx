1c029576960203e21fb7b8ee3fac00a021a34eb4
Oleg Cherr <olegcherr@gmail.com>
2023-01-09 15:19:50 +0100

    MailDetailsPage: Remove decoding process since message should be already decoded; Inline MailDetailsPageInner

--- src/pages/MailDetailsPage/MailDetailsPage.tsx
+++ src/pages/MailDetailsPage/MailDetailsPage.tsx
@@ -13,31 +13,27 @@ import { IMessageDecodedContent } from '../../indexedDB/MessagesDB';
 import { GenericLayout } from '../../layouts/GenericLayout';
 import contacts from '../../stores/Contacts';
 import mailbox from '../../stores/Mailbox';
-import { FolderId, ILinkedMessage, useMailStore } from '../../stores/MailList';
+import { FolderId, useMailStore } from '../../stores/MailList';
 import { EDITOR_JS_TOOLS } from '../../utils/editorJs';
 import { useNav } from '../../utils/navigate';
 
 const ReactEditorJS = createReactEditorJS();
 
-interface MailDetailsPageInnerProps {
-	message: ILinkedMessage;
-}
-
-const MailDetailsPageInner = observer(({ message }: MailDetailsPageInnerProps) => {
+export const MailDetailsPage = observer(() => {
 	const navigate = useNav();
-	const { lastActiveFolderId, decodedMessagesById, decodeMessage, markMessagesAsReaded, markMessagesAsDeleted } =
-		useMailStore();
-	const decoded: IMessageDecodedContent | undefined = decodedMessagesById[message.msgId];
-	const contact = contacts.contactsByAddress[message.msg.senderAddress || '-1'];
+	const { id } = useParams();
+
+	const { lastActiveFolderId, decodedMessagesById, markMessagesAsDeleted, lastMessagesList } = useMailStore();
+
+	const message = lastMessagesList.find(m => m.id === id!);
+	const decoded: IMessageDecodedContent | undefined = message && decodedMessagesById[message.msgId];
+	const contact = contacts.contactsByAddress[message?.msg.senderAddress || '-1'];
 
 	useEffect(() => {
-		(async () => {
-			if (!decoded) {
-				await decodeMessage(message);
-			}
-			await markMessagesAsReaded([message.id]);
-		})();
-	}, [decodeMessage, decoded, markMessagesAsReaded, message]);
+		if (!message || !decoded) {
+			navigate(`/mail/${FolderId.Inbox}`);
+		}
+	}, [decoded, message, navigate]);
 
 	const data = useMemo(
 		() => ({
@@ -50,14 +46,14 @@ const MailDetailsPageInner = observer(({ message }: MailDetailsPageInnerProps) =
 	);
 
 	const replyClickHandler = () => {
-		mailbox.to = message.msg.senderAddress
+		mailbox.to = message!.msg.senderAddress
 			? [
 					{
 						type: 'address',
 						loading: false,
 						isAchievable: null,
-						input: message.msg.senderAddress,
-						address: message.msg.senderAddress,
+						input: message!.msg.senderAddress,
+						address: message!.msg.senderAddress,
 					},
 			  ]
 			: [];
@@ -72,7 +68,7 @@ const MailDetailsPageInner = observer(({ message }: MailDetailsPageInnerProps) =
 	};
 
 	const deleteHandler = () => {
-		markMessagesAsDeleted([message]);
+		markMessagesAsDeleted([message!]);
 		navigate(`/${lastActiveFolderId}`);
 	};
 
@@ -83,92 +79,83 @@ const MailDetailsPageInner = observer(({ message }: MailDetailsPageInnerProps) =
 				link: `/mail/${lastActiveFolderId}`,
 			}}
 		>
-			<div className="mail-page animated fadeInRight">
-				<div className="mail-top">
-					<div className="mail-header">
-						<h2 className="mailbox-title">
-							{decoded ? decoded.decodedSubject || 'View Message' : 'View Message'}
-						</h2>
-						<div className="mail-actions">
-							<ActionButton
-								onClick={replyClickHandler}
-								icon={<i className={`fa ${smallButtonIcons.reply}`} />}
-							>
-								Reply
-							</ActionButton>
+			{message && decoded && (
+				<div className="mail-page animated fadeInRight">
+					<div className="mail-top">
+						<div className="mail-header">
+							<h2 className="mailbox-title">
+								{decoded ? decoded.decodedSubject || 'View Message' : 'View Message'}
+							</h2>
+							<div className="mail-actions">
+								<ActionButton
+									onClick={replyClickHandler}
+									icon={<i className={`fa ${smallButtonIcons.reply}`} />}
+								>
+									Reply
+								</ActionButton>
 
-							<ActionButton
-								style={ActionButtonStyle.Dengerous}
-								onClick={deleteHandler}
-								icon={<i className={`fa ${smallButtonIcons.trash}`} />}
-							>
-								Archive
-							</ActionButton>
-						</div>
-					</div>
-					<div className="mail-meta">
-						<div className="mail-params">
-							<div className="mmp-row">
-								<div className="mmp-row-title mmp-from">Sender:</div>
-								<div className="mmp-row-value">
-									{contact ? (
-										<div className="mail-contact-name">{contact.name}</div>
-									) : (
-										<div className="mail-sender">
-											<Blockie
-												className="mail-sender-blockie"
-												address={message.msg.senderAddress}
-											/>{' '}
-											<div className="mail-sender-address">
-												<AdaptiveAddress address={message.msg.senderAddress} />
-											</div>
-										</div>
-									)}
-								</div>
+								<ActionButton
+									style={ActionButtonStyle.Dengerous}
+									onClick={deleteHandler}
+									icon={<i className={`fa ${smallButtonIcons.trash}`} />}
+								>
+									Archive
+								</ActionButton>
 							</div>
 						</div>
-						<div className="mail-date">{moment.unix(message.msg.createdAt).format('HH:mm DD.MM.YYYY')}</div>
+						<div className="mail-meta">
+							<div className="mail-params">
+								<div className="mmp-row">
+									<div className="mmp-row-title mmp-from">Sender:</div>
+									<div className="mmp-row-value">
+										{contact ? (
+											<div className="mail-contact-name">{contact.name}</div>
+										) : (
+											<div className="mail-sender">
+												<Blockie
+													className="mail-sender-blockie"
+													address={message.msg.senderAddress}
+												/>{' '}
+												<div className="mail-sender-address">
+													<AdaptiveAddress address={message.msg.senderAddress} />
+												</div>
+											</div>
+										)}
+									</div>
+								</div>
+							</div>
+							<div className="mail-date">
+								{moment.unix(message.msg.createdAt).format('HH:mm DD.MM.YYYY')}
+							</div>
+						</div>
+					</div>
+					<div className="mail-body" style={{ minHeight: 370 }}>
+						{data.blocks && (
+							<ReactEditorJS
+								tools={EDITOR_JS_TOOLS}
+								readOnly={true}
+								//@ts-ignore
+								data={data}
+							/>
+						)}
+					</div>
+					<div className="mail-footer">
+						<ActionButton
+							onClick={replyClickHandler}
+							icon={<i className={`fa ${smallButtonIcons.reply}`} />}
+						>
+							Reply
+						</ActionButton>
+
+						<ActionButton
+							onClick={forwardClickHandler}
+							icon={<i className={`fa ${smallButtonIcons.forward}`} />}
+						>
+							Forward
+						</ActionButton>
 					</div>
 				</div>
-				<div className="mail-body" style={{ minHeight: 370 }}>
-					{data.blocks && (
-						<ReactEditorJS
-							tools={EDITOR_JS_TOOLS}
-							readOnly={true}
-							//@ts-ignore
-							data={data}
-						/>
-					)}
-				</div>
-				<div className="mail-footer">
-					<ActionButton onClick={replyClickHandler} icon={<i className={`fa ${smallButtonIcons.reply}`} />}>
-						Reply
-					</ActionButton>
-
-					<ActionButton
-						onClick={forwardClickHandler}
-						icon={<i className={`fa ${smallButtonIcons.forward}`} />}
-					>
-						Forward
-					</ActionButton>
-				</div>
-			</div>
+			)}
 		</GenericLayout>
 	);
 });
-
-export const MailDetailsPage = () => {
-	const navigate = useNav();
-	const { id } = useParams();
-
-	const lastMessagesList = useMailStore(state => state.lastMessagesList);
-	const message = lastMessagesList.find(m => m.id === id!);
-
-	useEffect(() => {
-		if (!message) {
-			navigate(`/mail/${FolderId.Inbox}`);
-		}
-	}, [message, navigate]);
-
-	return <>{message && <MailDetailsPageInner message={message} />}</>;
-};
