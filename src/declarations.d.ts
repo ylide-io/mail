a564d7152af2776f60c3c5e209e917be107cdc24
Oleg Cherr <olegcherr@gmail.com>
2023-01-13 10:20:57 +0100

    Add SVG declaration

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,6 +1,14 @@
+// noinspection JSUnusedGlobalSymbols
+
 declare module '*.module.scss' {
 	const css: { [key: string]: string };
 	export = css;
+}
+
+declare module '*.svg' {
+	export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+	const src: string;
+	export default src;
 }
 
 declare module '*.mp4' {
