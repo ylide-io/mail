d05fa6d0bd236a6977c39f85862d75ce732ae7d0
Oleg Cherr <olegcherr@gmail.com>
2023-01-27 13:49:24 +0100

    Fix closing tx-popup

--- src/layouts/GenericLayout.tsx
+++ src/layouts/GenericLayout.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { observer } from 'mobx-react';
 import React, { ReactNode, useEffect } from 'react';
 
 import Header from '../components/Header/Header';
@@ -18,40 +19,42 @@ interface GenericLayoutProps {
 	};
 }
 
-export const GenericLayout = ({ children, mainClass, isCustomContent, mobileTopButtonProps }: GenericLayoutProps) => {
-	useEffect(() => {
-		setTimeout(() => {
-			// @ts-ignore
-			window.WonderPush = window.WonderPush || [];
-			// @ts-ignore
-			WonderPush.push([
-				'init',
-				{
-					webKey: '0ae1e313db96a1e743f93f73f2130261b6efd43953263299400632d700599fa6',
-				},
-			]);
-		}, 10000);
-	}, []);
+export const GenericLayout = observer(
+	({ children, mainClass, isCustomContent, mobileTopButtonProps }: GenericLayoutProps) => {
+		useEffect(() => {
+			setTimeout(() => {
+				// @ts-ignore
+				window.WonderPush = window.WonderPush || [];
+				// @ts-ignore
+				WonderPush.push([
+					'init',
+					{
+						webKey: '0ae1e313db96a1e743f93f73f2130261b6efd43953263299400632d700599fa6',
+					},
+				]);
+			}, 10000);
+		}, []);
 
-	return (
-		<div className={css.root}>
-			<Header />
-			{domain.txPlateVisible ? <TransactionPopup /> : null}
-			<div className={css.main}>
-				<SidebarMenu />
+		return (
+			<div className={css.root}>
+				<Header />
+				{domain.txPlateVisible ? <TransactionPopup /> : null}
+				<div className={css.main}>
+					<SidebarMenu />
 
-				<div className={clsx(css.content, isCustomContent && css.content_custom, mainClass)}>
-					{!!mobileTopButtonProps && (
-						<LinkButton
-							className={css.linkButton}
-							text={mobileTopButtonProps.text}
-							link={mobileTopButtonProps.link}
-						/>
-					)}
+					<div className={clsx(css.content, isCustomContent && css.content_custom, mainClass)}>
+						{!!mobileTopButtonProps && (
+							<LinkButton
+								className={css.linkButton}
+								text={mobileTopButtonProps.text}
+								link={mobileTopButtonProps.link}
+							/>
+						)}
 
-					{children}
+						{children}
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	},
+);
