dd530b1262b5313927ed1e8625a872b54711e1f4
Oleg Cherr <olegcherr@gmail.com>
2023-02-03 11:13:36 +0100

    Mail: Pass mail-data to ComposeMailForm/Popup explicitly

--- src/components/composeMailForm/composeMailForm.tsx
+++ src/components/composeMailForm/composeMailForm.tsx
@@ -1,36 +1,37 @@
 import clsx from 'clsx';
-import React, { useState } from 'react';
+import { observer } from 'mobx-react';
+import React from 'react';
 
-import { globalOutgoingMailData } from '../../stores/outgoingMailData';
+import { OutgoingMailData } from '../../stores/outgoingMailData';
 import { formatSubject } from '../../utils/mail';
 import { AccountSelect } from '../accountSelect/accountSelect';
 import { PropsWithClassName } from '../propsWithClassName';
-import { RecipientInput, RecipientInputItem } from '../recipientInput/recipientInput';
+import { RecipientInput } from '../recipientInput/recipientInput';
 import { TextField } from '../textField/textField';
 import css from './composeMailForm.module.scss';
 
-export interface ComposeMailFormProps extends PropsWithClassName {}
+export interface ComposeMailFormProps extends PropsWithClassName {
+	mailData: OutgoingMailData;
+}
 
-export function ComposeMailForm({ className }: ComposeMailFormProps) {
-	const [recipients, setRecipients] = useState<RecipientInputItem[]>([]);
-
+export const ComposeMailForm = observer(({ className, mailData }: ComposeMailFormProps) => {
 	return (
 		<div className={clsx(css.root, className)}>
 			<div className={css.meta}>
 				<div className={css.metaLabel}>From</div>
-				<AccountSelect
-					activeAccount={globalOutgoingMailData.from}
-					onChange={account => (globalOutgoingMailData.from = account)}
-				/>
+				<AccountSelect activeAccount={mailData.from} onChange={account => (mailData.from = account)} />
 
 				<div className={css.metaLabel}>To</div>
-				<RecipientInput initialValue={globalOutgoingMailData.to} onChange={setRecipients} />
+				<RecipientInput
+					initialValue={mailData.to}
+					onChange={value => (mailData.to = value.map(it => it.routing?.address).filter(Boolean) as string[])}
+				/>
 
 				<div className={css.metaLabel}>Subject</div>
 				<TextField
 					placeholder={formatSubject()}
-					value={globalOutgoingMailData.subject}
-					onChange={value => (globalOutgoingMailData.subject = value)}
+					value={mailData.subject}
+					onChange={value => (mailData.subject = value)}
 				/>
 			</div>
 
@@ -39,4 +40,4 @@ export function ComposeMailForm({ className }: ComposeMailFormProps) {
 			<div className={css.footer}>FOOTER</div>
 		</div>
 	);
-}
+});
--- src/components/composeMailPopup/composeMailPopup.tsx
+++ src/components/composeMailPopup/composeMailPopup.tsx
@@ -2,24 +2,34 @@ import clsx from 'clsx';
 import React from 'react';
 
 import { ReactComponent as CrossSvg } from '../../icons/cross.svg';
+import { OutgoingMailData } from '../../stores/outgoingMailData';
 import { useOnMountAnimation } from '../../utils/useOnMountAnimation';
 import { ComposeMailForm } from '../composeMailForm/composeMailForm';
 import { Popup } from '../popup/popup';
 import { useStaticComponentManager } from '../staticComponentManager/staticComponentManager';
 import css from './composeMailPopup.module.scss';
 
-export function useComposeMailPopup() {
-	const staticComponentManager = useStaticComponentManager();
-
-	return () => staticComponentManager.show(onRemove => <ComposeMailPopup onClose={onRemove} />);
-}
-
 export interface ComposeMailPopupProps {
-	// mailData: OutgoingMailData;
+	mailData: OutgoingMailData;
 	onClose?: () => void;
 }
 
-export function ComposeMailPopup({ onClose }: ComposeMailPopupProps) {
+export function useComposeMailPopup() {
+	const staticComponentManager = useStaticComponentManager();
+
+	return (props: ComposeMailPopupProps) =>
+		staticComponentManager.show(onRemove => (
+			<ComposeMailPopup
+				{...props}
+				onClose={() => {
+					onRemove();
+					props.onClose?.();
+				}}
+			/>
+		));
+}
+
+export function ComposeMailPopup({ onClose, mailData }: ComposeMailPopupProps) {
 	const isMount = useOnMountAnimation();
 
 	return (
@@ -35,7 +45,7 @@ export function ComposeMailPopup({ onClose }: ComposeMailPopupProps) {
 				</button>
 			</div>
 
-			<ComposeMailForm className={css.form} />
+			<ComposeMailForm className={css.form} mailData={mailData} />
 		</Popup>
 	);
 }
