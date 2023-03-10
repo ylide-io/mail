c35fbf3e0f58e7db7b8c5c6f95aced3eec041fe7
Oleg Cherr <olegcherr@gmail.com>
2023-03-10 14:49:49 +0100

    SendMessageWidget: Allow using without initial address

--- src/pages/widgets/sendMessageWidget/sendMessageWidget.tsx
+++ src/pages/widgets/sendMessageWidget/sendMessageWidget.tsx
@@ -5,18 +5,16 @@ import { ActionButton, ActionButtonLook } from '../../../components/ActionButton
 import { Recipients } from '../../../components/recipientInput/recipientInput';
 import { ReactComponent as CrossSvg } from '../../../icons/ic20/cross.svg';
 import { OutgoingMailData } from '../../../stores/outgoingMailData';
-import { invariant } from '../../../utils/assert';
 import { ComposeMailForm } from '../../mail/components/composeMailForm/composeMailForm';
 import css from './sendMessageWidget.module.scss';
 
 export function SendMessageWidget() {
 	const [searchParams] = useSearchParams();
 	const toAddress = searchParams.get('to');
-	invariant(toAddress, 'to-address required');
 
 	const mailData = useMemo(() => {
 		const data = new OutgoingMailData();
-		data.to = new Recipients([toAddress]);
+		data.to = new Recipients(toAddress ? [toAddress] : undefined);
 		return data;
 	}, [toAddress]);
 
