18388f754fe200a6a259360386619e360428f977
Oleg Cherr <olegcherr@gmail.com>
2023-03-13 13:10:09 +0100

    SM Widget: Make TO-address required again

--- src/pages/widgets/sendMessageWidget/sendMessageWidget.tsx
+++ src/pages/widgets/sendMessageWidget/sendMessageWidget.tsx
@@ -5,12 +5,14 @@ import { ActionButton, ActionButtonLook } from '../../../components/ActionButton
 import { Recipients } from '../../../components/recipientInput/recipientInput';
 import { ReactComponent as CrossSvg } from '../../../icons/ic20/cross.svg';
 import { OutgoingMailData } from '../../../stores/outgoingMailData';
+import { invariant } from '../../../utils/assert';
 import { ComposeMailForm } from '../../mail/components/composeMailForm/composeMailForm';
 import css from './sendMessageWidget.module.scss';
 
 export function SendMessageWidget() {
 	const [searchParams] = useSearchParams();
 	const toAddress = searchParams.get('to');
+	invariant(toAddress, 'to-address required');
 
 	const mailData = useMemo(() => {
 		const data = new OutgoingMailData();
