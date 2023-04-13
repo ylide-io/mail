47491a9311c703cc0560985bf3d64af92fc7a9d6
Oleg Cherr <olegcherr@gmail.com>
2023-04-13 08:58:25 +0200

    Improve 'formatAddress' (support Everscale; convert to lower case)

--- src/utils/blockchain.tsx
+++ src/utils/blockchain.tsx
@@ -27,7 +27,14 @@ export function isAddress(input: string): boolean {
 }
 
 export function formatAddress(address: string) {
-	return address.replace(/^0{24}/, '0x');
+	return (
+		address
+			.toLowerCase()
+			// 000000000000000000000000d3c2b7b1ebcd6949abcf1041cc629b2648ad2329 -> 0xd3c2b7b1ebcd6949abcf1041cc629b2648ad2329
+			.replace(/^0{24}/, '0x')
+			// 3f4dcce76d4760fb23879fee1a28f278b0739f99c13911094418c042fb7fbc45 -> 0:3f4dcce76d4760fb23879fee1a28f278b0739f99c13911094418c042fb7fbc45
+			.replace(/^([a-f0-9]{64})$/i, '0:$1')
+	);
 }
 
 //
