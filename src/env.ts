6bdfc6b30eba3bfc751f758ba6ccc60308d4ea6f
Oleg Cherr <olegcherr@gmail.com>
2023-05-04 13:00:34 +0200

    Add REACT_APP__SMART_FEED_MODE env

--- src/env.ts
+++ src/env.ts
@@ -4,3 +4,5 @@ export const REACT_APP__FEED_SERVER = process.env.REACT_APP__FEED_SERVER;
 
 export const REACT_APP__OTC_MODE = !!process.env.REACT_APP__OTC_MODE;
 export const REACT_APP__OTC_PROVIDER = (process.env.REACT_APP__OTC_PROVIDER as OtcProvider) || OtcProvider.AIRSWAP;
+
+export const REACT_APP__SMART_FEED_MODE = !!process.env.REACT_APP__SMART_FEED_MODE;
