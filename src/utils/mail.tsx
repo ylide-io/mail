3f284541af115e86cfea223ea93361b5aa829fe1
Oleg Cherr <olegcherr@gmail.com>
2023-05-24 14:59:47 +0200

    Simplify 'sendMessage' a bit

--- src/utils/mail.tsx
+++ src/utils/mail.tsx
@@ -48,69 +48,64 @@ export async function sendMessage(
 	network?: EVMNetwork,
 	feedId?: Uint256,
 ): Promise<SendMailResult | null> {
-	let error = false;
-
 	analytics.mailSentAttempt();
 
-	try {
-		const secureContext = MessageSecureContext.create();
-		const ipfsStorage = new YlideIpfsStorage();
+	const secureContext = MessageSecureContext.create();
+	const ipfsStorage = new YlideIpfsStorage();
 
-		const messageAttachments = await Promise.all(
-			attachments.map(async file => {
-				const buffer = await readFileAsArrayBuffer(file);
-				const uint8Array = new Uint8Array(buffer);
-				const encrypted = secureContext.encrypt(uint8Array);
-				const uploaded = await ipfsStorage.uploadToIpfs(encrypted);
+	const messageAttachments = await Promise.all(
+		attachments.map(async file => {
+			const buffer = await readFileAsArrayBuffer(file);
+			const uint8Array = new Uint8Array(buffer);
+			const encrypted = secureContext.encrypt(uint8Array);
+			const uploaded = await ipfsStorage.uploadToIpfs(encrypted);
 
-				return new MessageAttachmentLinkV1({
-					type: MessageAttachmentType.LINK_V1,
-					previewLink: '',
-					link: `ipfs://${uploaded.hash}`,
-					fileName: file.name,
-					fileSize: file.size,
-					isEncrypted: true,
-				});
-			}),
-		);
+			return new MessageAttachmentLinkV1({
+				type: MessageAttachmentType.LINK_V1,
+				previewLink: '',
+				link: `ipfs://${uploaded.hash}`,
+				fileName: file.name,
+				fileSize: file.size,
+				isEncrypted: true,
+			});
+		}),
+	);
 
-		const content = new MessageContentV4({
-			sendingAgentName: 'ysh',
-			sendingAgentVersion: { major: 1, minor: 0, patch: 0 },
-			subject,
-			content: text,
-			attachments: messageAttachments,
-			extraBytes: new Uint8Array(0),
-			extraJson: {},
-		});
+	const content = new MessageContentV4({
+		sendingAgentName: 'ysh',
+		sendingAgentVersion: { major: 1, minor: 0, patch: 0 },
+		subject,
+		content: text,
+		attachments: messageAttachments,
+		extraBytes: new Uint8Array(0),
+		extraJson: {},
+	});
 
-		if (!network && sender.wallet.factory.blockchainGroup === 'evm') {
-			network = await getEvmWalletNetwork(sender.wallet);
-		}
-
-		return await domain.ylide.core.sendMessage(
-			{
-				wallet: sender.wallet.controller,
-				sender: sender.account,
-				content,
-				recipients,
-				secureContext,
-				serviceCode: ServiceCode.MAIL,
-				feedId,
-			},
-			{
-				network,
-			},
-		);
-	} catch (e) {
-		error = true;
-		throw e;
-	} finally {
-		if (!error) {
-			analytics.mailSentSuccessful();
-		}
+	if (!network && sender.wallet.factory.blockchainGroup === 'evm') {
+		network = await getEvmWalletNetwork(sender.wallet);
 	}
+
+	const result = await domain.ylide.core.sendMessage(
+		{
+			wallet: sender.wallet.controller,
+			sender: sender.account,
+			content,
+			recipients,
+			secureContext,
+			serviceCode: ServiceCode.MAIL,
+			feedId,
+		},
+		{
+			network,
+		},
+	);
+
+	analytics.mailSentSuccessful();
+
+	return result;
 }
+
+export async function b() {}
 
 //
 
