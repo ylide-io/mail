20e8cad6573aa6b1e57dcb54f9c522e0ab1c71e3
Oleg Cherr <olegcherr@gmail.com>
2023-06-09 16:24:56 +0200

    Refactor 'connectAccount' helper for a bit

--- src/utils/account.tsx
+++ src/utils/account.tsx
@@ -10,7 +10,6 @@ import { toast } from '../components/toast/toast';
 import domain from '../stores/Domain';
 import { DomainAccount } from '../stores/models/DomainAccount';
 import { Wallet } from '../stores/models/Wallet';
-import { invariant } from './assert';
 import { getQueryString } from './getQueryString';
 import { truncateInMiddle } from './string';
 import { walletsMeta } from './wallet';
@@ -26,103 +25,110 @@ export function formatAccountName(account: DomainAccount) {
 //
 
 export async function connectAccount(): Promise<DomainAccount | undefined> {
-	let wallet: Wallet | undefined;
-	const proxyAccount = domain.availableProxyAccounts[0];
+	try {
+		let wallet: Wallet | undefined;
+		const proxyAccount = domain.availableProxyAccounts[0];
 
-	if (proxyAccount) {
-		const useProxy = await showStaticComponent<boolean>(resolve => (
-			<ActionModal
-				title="Connect same account?"
-				description={
-					<>
-						We noticed that you're using Ylide within another application. You can connect the same account
-						as the parent application uses –{' '}
-						<b>{truncateInMiddle(proxyAccount.account.address, 8, '...')}</b>
-						<br />
-						<br />
-						We recommend connecting the same account to get seamless user experience.
-					</>
-				}
-				buttons={[
-					<ActionButton
-						size={ActionButtonSize.XLARGE}
-						look={ActionButtonLook.PRIMARY}
-						onClick={() => resolve(true)}
-					>
-						Connect same account
-					</ActionButton>,
-					<ActionButton
-						size={ActionButtonSize.XLARGE}
-						look={ActionButtonLook.LITE}
-						onClick={() => resolve(false)}
-					>
-						Use another one
-					</ActionButton>,
-				]}
-				onClose={resolve}
-			/>
-		));
+		if (proxyAccount) {
+			const useProxy = await showStaticComponent<boolean>(resolve => (
+				<ActionModal
+					title="Connect same account?"
+					description={
+						<>
+							We noticed that you're using Ylide within another application. You can connect the same
+							account as the parent application uses –{' '}
+							<b>{truncateInMiddle(proxyAccount.account.address, 8, '...')}</b>
+							<br />
+							<br />
+							We recommend connecting the same account to get seamless user experience.
+						</>
+					}
+					buttons={[
+						<ActionButton
+							size={ActionButtonSize.XLARGE}
+							look={ActionButtonLook.PRIMARY}
+							onClick={() => resolve(true)}
+						>
+							Connect same account
+						</ActionButton>,
+						<ActionButton
+							size={ActionButtonSize.XLARGE}
+							look={ActionButtonLook.LITE}
+							onClick={() => resolve(false)}
+						>
+							Use another one
+						</ActionButton>,
+					]}
+					onClose={resolve}
+				/>
+			));
 
-		if (useProxy == null) {
-			return;
-		} else if (useProxy) {
-			wallet = proxyAccount.wallet;
+			if (useProxy == null) {
+				return;
+			} else if (useProxy) {
+				wallet = proxyAccount.wallet;
+			}
 		}
-	}
 
-	if (!wallet) {
-		wallet = await showStaticComponent<Wallet>(resolve => <SelectWalletModal onClose={resolve} />);
-	}
+		if (!wallet) {
+			wallet = await showStaticComponent<Wallet>(resolve => <SelectWalletModal onClose={resolve} />);
+		}
 
-	if (wallet) {
-		async function connectWalletAccount(wallet: Wallet) {
-			const walletMeta = walletsMeta[wallet.wallet];
-			let currentAccount = await wallet.getCurrentAccount();
-			// to fix everwallet stuck in auth state without registered key
-			if (
-				currentAccount &&
-				!wallet.isAccountRegistered(currentAccount) &&
-				!wallet.controller.isMultipleAccountsSupported() &&
-				!walletMeta.isProxy
-			) {
-				await wallet.controller.disconnectAccount(currentAccount);
-			}
-			currentAccount = await wallet.getCurrentAccount();
-			if (currentAccount && wallet.isAccountRegistered(currentAccount)) {
-				const result = await SwitchModal.show('account', wallet);
-				if (!result) return;
-			}
-			currentAccount = await wallet.getCurrentAccount();
-			if (currentAccount && wallet.isAccountRegistered(currentAccount)) {
-				const domainAccount = wallet.accounts.find(a => a.account.address === currentAccount!.address)!;
-				if (domainAccount.isLocalKeyRegistered) {
-					toast('This account is already connected. Please choose a different one.');
-					return;
-				} else {
-					await domain.accounts.removeAccount(domainAccount);
-					return await wallet.connectAccount();
+		if (wallet) {
+			async function connectWalletAccount(wallet: Wallet) {
+				const walletMeta = walletsMeta[wallet.wallet];
+
+				let currentAccount = await wallet.getCurrentAccount();
+
+				// to fix everwallet stuck in auth state without registered key
+				if (
+					currentAccount &&
+					!wallet.isAccountRegistered(currentAccount) &&
+					!wallet.controller.isMultipleAccountsSupported() &&
+					!walletMeta.isProxy
+				) {
+					await wallet.controller.disconnectAccount(currentAccount);
+					currentAccount = await wallet.getCurrentAccount();
 				}
-			} else {
+
+				if (currentAccount && wallet.isAccountRegistered(currentAccount)) {
+					const result = await SwitchModal.show('account', wallet);
+					if (!result) return;
+
+					currentAccount = await wallet.getCurrentAccount();
+				}
+
+				if (currentAccount && wallet.isAccountRegistered(currentAccount)) {
+					const domainAccount = wallet.accounts.find(a => a.account.address === currentAccount!.address)!;
+					if (domainAccount.isLocalKeyRegistered) {
+						return toast('This account is already connected. Please choose a different one.');
+					} else {
+						await domain.accounts.removeAccount(domainAccount);
+					}
+				}
+
 				return await wallet.connectAccount();
 			}
+
+			const account = await connectWalletAccount(wallet);
+			if (!account) return;
+
+			const remoteKeys = await wallet.readRemoteKeys(account);
+			const qqs = getQueryString();
+
+			return await showStaticComponent<DomainAccount>(resolve => (
+				<NewPasswordModal
+					faucetType={['polygon', 'fantom', 'gnosis'].includes(qqs.faucet) ? (qqs.faucet as any) : 'gnosis'}
+					bonus={qqs.bonus === 'true'}
+					wallet={wallet!}
+					account={account}
+					remoteKeys={remoteKeys.remoteKeys}
+					onClose={resolve}
+				/>
+			));
 		}
-
-		const account = await connectWalletAccount(wallet);
-		invariant(account);
-
-		const remoteKeys = await wallet.readRemoteKeys(account);
-		const qqs = getQueryString();
-
-		return await showStaticComponent<DomainAccount>(resolve => (
-			<NewPasswordModal
-				faucetType={['polygon', 'fantom', 'gnosis'].includes(qqs.faucet) ? (qqs.faucet as any) : 'gnosis'}
-				bonus={qqs.bonus === 'true'}
-				wallet={wallet!}
-				account={account}
-				remoteKeys={remoteKeys.remoteKeys}
-				onClose={resolve}
-			/>
-		));
+	} catch (e) {
+		console.error(e);
 	}
 }
 
