25d3d85e70e9be4eb52784a0b6fa37745afda8fa
Danila Simonov <ek@ek-mail.ru>
2023-06-26 17:02:57 +0200

    feedId in CreatePostForm, unique feedIds for different projects

--- src/pages/feed/feedPage/feedPage.tsx
+++ src/pages/feed/feedPage/feedPage.tsx
@@ -258,6 +258,7 @@ const VenomFeedContent = observer(() => {
 			{venomAccounts.length ? (
 				<CreatePostForm
 					ref={createPostFormRef}
+					feedId={projectMeta.feedId}
 					className={css.createPostForm}
 					accounts={venomAccounts}
 					isAnavailable={serviceStatus.data !== 'ACTIVE'}
