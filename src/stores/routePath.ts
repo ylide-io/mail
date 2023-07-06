66210041bb5178f397b455313fed5dd369d7fe9a
ksamarin90 <konstantin.samarin@roci.fi>
2023-07-06 17:20:09 +0400

    fix admin

--- src/stores/routePath.ts
+++ src/stores/routePath.ts
@@ -27,7 +27,7 @@ export enum RoutePath {
 	FEED_SMART = '/feed/smart',
 	FEED_SMART_ADDRESS = '/feed/smart/:address',
 	FEED_VENOM = '/feed/venom',
-	FEED_VENOM_ADMIN = '/feed/venom/admin',
+	FEED_VENOM_ADMIN = '/feed/venom/:project/admin',
 	FEED_VENOM_PROJECT = '/feed/venom/:project',
 
 	OTC_ASSETS = '/otc/assets',
