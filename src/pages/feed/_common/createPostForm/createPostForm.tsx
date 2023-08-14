728ded405cb1ccdcf79423c4d33599d93bab7389
Danila Simonov <ek@ek-mail.ru>
2023-08-14 16:57:56 +0200

    cronos fix

--- src/pages/feed/_common/createPostForm/createPostForm.tsx
+++ src/pages/feed/_common/createPostForm/createPostForm.tsx
@@ -118,7 +118,7 @@ export const CreatePostForm = observer(
 				let cancelled = false;
 				BlockchainFeedApi.getComissions({ feedId: mailData.feedId })
 					.then(comissions => {
-						if (cancelled || !mailData.from) {
+						if (cancelled || !mailData.from || !mailData.network) {
 							return;
 						}
 						const blockchain = domain.getBlockchainName(mailData.network);
