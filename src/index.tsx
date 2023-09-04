354231a4cda31f56dc6011d3687d6f8955bb32df
Oleg Cherr <olegcherr@gmail.com>
2023-09-04 13:46:08 +0200

    Update and fix Sentry config

--- src/index.tsx
+++ src/index.tsx
@@ -2,31 +2,14 @@ import 'normalize.css';
 import 'minireset.css';
 import './styles/index.scss';
 
-import * as Sentry from '@sentry/react';
 import { configure } from 'mobx';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import packageJson from '../package.json';
 import { App } from './App';
+import { initSentry } from './utils/sentry';
 
-if (document.location.hostname !== 'localhost') {
-	Sentry.init({
-		dsn: 'https://4e8da33932b3e6ec11ad2d95738007ae@o4504063808110592.ingest.sentry.io/4505709214302208',
-		integrations: [new Sentry.BrowserTracing()],
-		release: packageJson.version,
-		environment: ['hub.ylide.io', 'app.mainview.io'].includes(document.location.hostname)
-			? 'production'
-			: ['staging.ylide.io'].includes(document.location.hostname)
-			? 'staging'
-			: 'local',
-
-		// Set tracesSampleRate to 1.0 to capture 100%
-		// of transactions for performance monitoring.
-		// We recommend adjusting this value in production
-		tracesSampleRate: 1.0,
-	});
-}
+initSentry();
 
 configure({
 	enforceActions: 'never',
---
+++ src/utils/sentry.ts
@@ -0,0 +1,36 @@
+import * as Sentry from '@sentry/react';
+
+import packageJson from '../../package.json';
+import { AppMode, REACT_APP__APP_MODE } from '../env';
+
+export function initSentry() {
+	const dsn = {
+		[AppMode.HUB]: 'https://4c4f62731ece4b1e9373a5ef48e6ff9b@o4504063808110592.ingest.sentry.io/4504063811452929',
+		[AppMode.MAIN_VIEW]:
+			'https://4e8da33932b3e6ec11ad2d95738007ae@o4504063808110592.ingest.sentry.io/4505709214302208',
+		[AppMode.OTC]: '',
+	}[REACT_APP__APP_MODE];
+
+	if (!dsn) return;
+
+	const hostname = document.location.hostname;
+	const environment = ['hub.ylide.io', 'app.mainview.io'].includes(hostname)
+		? 'production'
+		: ['staging.ylide.io', 'staging.mainview.io'].includes(hostname)
+		? 'staging'
+		: 'local';
+
+	if (environment === 'local') return;
+
+	Sentry.init({
+		dsn,
+		integrations: [new Sentry.BrowserTracing()],
+		release: packageJson.version,
+		environment,
+
+		// Set tracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production
+		tracesSampleRate: 1.0,
+	});
+}
