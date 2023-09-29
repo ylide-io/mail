fbacecd998d93891d9a920713430421760bcafb4
Oleg Cherr <olegcherr@gmail.com>
2023-09-29 10:50:32 +0200

    Extract post reactions-functionality to PostReactions

--- src/pages/community/_common/discussionPost/discussionPost.tsx
+++ src/pages/community/_common/discussionPost/discussionPost.tsx
@@ -16,10 +16,8 @@ import { Avatar } from '../../../../components/avatar/avatar';
 import { BlockChainLabel } from '../../../../components/BlockChainLabel/BlockChainLabel';
 import { GridRowBox } from '../../../../components/boxes/boxes';
 import { GalleryModal } from '../../../../components/galleryModal/galleryModal';
-import { ReactionBadge } from '../../../../components/reactionBadge/reactionBadge';
 import { ReadableDate } from '../../../../components/readableDate/readableDate';
 import { Recipients } from '../../../../components/recipientInput/recipientInput';
-import { SetReactionFlow } from '../../../../components/setReactionFlow/setReactionFlow';
 import { Spinner, SpinnerLook } from '../../../../components/spinner/spinner';
 import { TextProcessor } from '../../../../components/textProcessor/textProcessor';
 import { toast } from '../../../../components/toast/toast';
@@ -27,24 +25,21 @@ import { ReactComponent as CrossSvg } from '../../../../icons/ic20/cross.svg';
 import { ReactComponent as ExternalSvg } from '../../../../icons/ic20/external.svg';
 import { ReactComponent as MailSvg } from '../../../../icons/ic20/mail.svg';
 import { ReactComponent as ReplySvg } from '../../../../icons/ic20/reply.svg';
-import { ReactComponent as SmileSvg } from '../../../../icons/ic20/smile.svg';
 import { ReactComponent as TickSvg } from '../../../../icons/ic20/tick.svg';
 import { MessageDecodedTextDataType } from '../../../../indexedDB/IndexedDB';
 import { analytics } from '../../../../stores/Analytics';
 import { browserStorage } from '../../../../stores/browserStorage';
 import { Community, CommunityId } from '../../../../stores/communities/communities';
 import domain from '../../../../stores/Domain';
-import { DomainAccount } from '../../../../stores/models/DomainAccount';
 import { OutgoingMailData } from '../../../../stores/outgoingMailData';
 import { RoutePath } from '../../../../stores/routePath';
-import { invariant } from '../../../../utils/assert';
 import { generateBlockchainExplorerUrl } from '../../../../utils/blockchain';
 import { copyToClipboard } from '../../../../utils/clipboard';
 import { getIpfsHashFromUrl, ipfsToHttpUrl } from '../../../../utils/ipfs';
 import { useOpenMailCompose } from '../../../../utils/mail';
-import { getAccountsForReaction } from '../../../../utils/reactions';
 import { useNav } from '../../../../utils/url';
 import { stickerIpfsIds } from '../createPostForm/stickerIpfsIds';
+import { PostReactions } from '../postReactions/postReactions';
 import { RepliedDiscussionPost } from '../repliedDiscussionPost/repliedDiscussionPost';
 import css from './discussionPost.module.scss';
 
@@ -134,62 +129,6 @@ export const DiscussionPost = observer(({ post: initialPost, community, onReplyC
 		mailData.to = new Recipients([post.msg.senderAddress]);
 
 		openMailCompose({ mailData, place: 'project-discussion' });
-	};
-
-	//
-
-	const reactionsCountsEntries = Object.entries(post.original.reactionsCounts);
-
-	const setReactionMutation = useMutation({
-		mutationFn: async (variables: { reaction: string; account: DomainAccount }) => {
-			invariant(variables.account.authKey, 'No auth key');
-
-			await BlockchainFeedApi.setReaction({
-				postId: post.msg.msgId,
-				reaction: variables.reaction,
-				authKey: variables.account.authKey,
-			});
-
-			await reloadPostMutation.mutateAsync();
-		},
-		onError: error => {
-			toast(`Couldn't set reaction 😟`);
-			console.error(error);
-		},
-	});
-
-	const setReaction = async (reaction: string, account: DomainAccount) => {
-		if (setReactionMutation.isLoading || removeReactionMutation.isLoading) return;
-
-		await setReactionMutation.mutateAsync({ reaction, account });
-	};
-
-	const removeReactionMutation = useMutation({
-		mutationFn: async (variables: { accounts: DomainAccount[] }) => {
-			await Promise.all(
-				variables.accounts
-					.filter(account => account.authKey && post.original.addressReactions[account.account.address])
-					.map(account =>
-						BlockchainFeedApi.setReaction({
-							postId: post.msg.msgId,
-							reaction: null,
-							authKey: account.authKey,
-						}),
-					),
-			);
-
-			await reloadPostMutation.mutateAsync();
-		},
-		onError: error => {
-			toast(`Couldn't remove reaction 😟`);
-			console.error(error);
-		},
-	});
-
-	const removeReaction = async (accounts: DomainAccount[]) => {
-		if (setReactionMutation.isLoading || removeReactionMutation.isLoading) return;
-
-		await removeReactionMutation.mutateAsync({ accounts });
 	};
 
 	//
@@ -401,71 +340,15 @@ export const DiscussionPost = observer(({ post: initialPost, community, onReplyC
 							<MailSvg />
 						</button>
 
-						<SetReactionFlow onSelect={setReaction}>
-							{({ anchorRef, onAnchorClick }) => (
-								<button
-									ref={anchorRef}
-									className={clsx(css.actionItem, css.actionItem_icon, css.actionItem_interactive)}
-									title="Reactions"
-									onClick={() => onAnchorClick()}
-								>
-									{setReactionMutation.isLoading || removeReactionMutation.isLoading ? (
-										<Spinner size={16} />
-									) : (
-										<SmileSvg />
-									)}
-								</button>
+						<PostReactions
+							post={post}
+							reloadPost={reloadPostMutation.mutateAsync}
+							reactionButtonClassName={clsx(
+								css.actionItem,
+								css.actionItem_icon,
+								css.actionItem_interactive,
 							)}
-						</SetReactionFlow>
-
-						{reactionsCountsEntries.length ? (
-							<div className={css.reactions}>
-								{reactionsCountsEntries.map(([reaction, count]) => {
-									const accountsForReaction = getAccountsForReaction(
-										reaction,
-										post.original.addressReactions,
-									);
-
-									return accountsForReaction.length ? (
-										<ReactionBadge
-											reaction={reaction}
-											counter={count || 1}
-											isActive
-											onClick={() => removeReaction(accountsForReaction)}
-										/>
-									) : (
-										<SetReactionFlow
-											key={reaction}
-											initialReaction={reaction}
-											onSelect={setReaction}
-										>
-											{({ anchorRef, onAnchorClick }) => (
-												<ReactionBadge
-													ref={anchorRef}
-													reaction={reaction}
-													counter={count || 1}
-													onClick={onAnchorClick}
-												/>
-											)}
-										</SetReactionFlow>
-									);
-								})}
-							</div>
-						) : (
-							<div className={css.noReactions}>
-								{['❤️', '👍', '🎉'].map(reaction => (
-									<SetReactionFlow key={reaction} initialReaction={reaction} onSelect={setReaction}>
-										{({ anchorRef, onAnchorClick }) => (
-											<ReactionBadge
-												ref={anchorRef}
-												reaction={reaction}
-												onClick={onAnchorClick}
-											/>
-										)}
-									</SetReactionFlow>
-								))}
-							</div>
-						)}
+						/>
 					</div>
 				</div>
 			</div>
---
+++ src/pages/community/_common/postReactions/postReactions.tsx
@@ -0,0 +1,134 @@
+import { observer } from 'mobx-react';
+import { useMutation } from 'react-query';
+
+import { BlockchainFeedApi, DecodedBlockchainFeedPost } from '../../../../api/blockchainFeedApi';
+import { ReactionBadge } from '../../../../components/reactionBadge/reactionBadge';
+import { SetReactionFlow } from '../../../../components/setReactionFlow/setReactionFlow';
+import { Spinner } from '../../../../components/spinner/spinner';
+import { toast } from '../../../../components/toast/toast';
+import { ReactComponent as SmileSvg } from '../../../../icons/ic20/smile.svg';
+import { DomainAccount } from '../../../../stores/models/DomainAccount';
+import { invariant } from '../../../../utils/assert';
+import { getAccountsForReaction } from '../../../../utils/reactions';
+import css from './postReactions.module.scss';
+
+export interface PostReactionsProps {
+	reactionButtonClassName: string;
+	post: DecodedBlockchainFeedPost;
+	reloadPost: () => Promise<void>;
+}
+
+export const PostReactions = observer(({ reactionButtonClassName, post, reloadPost }: PostReactionsProps) => {
+	const reactionsCountsEntries = Object.entries(post.original.reactionsCounts);
+
+	const setReactionMutation = useMutation({
+		mutationFn: async (variables: { reaction: string; account: DomainAccount }) => {
+			invariant(variables.account.authKey, 'No auth key');
+
+			await BlockchainFeedApi.setReaction({
+				postId: post.msg.msgId,
+				reaction: variables.reaction,
+				authKey: variables.account.authKey,
+			});
+
+			await reloadPost();
+		},
+		onError: error => {
+			toast(`Couldn't set reaction 😟`);
+			console.error(error);
+		},
+	});
+
+	const setReaction = async (reaction: string, account: DomainAccount) => {
+		if (setReactionMutation.isLoading || removeReactionMutation.isLoading) return;
+
+		await setReactionMutation.mutateAsync({ reaction, account });
+	};
+
+	const removeReactionMutation = useMutation({
+		mutationFn: async (variables: { accounts: DomainAccount[] }) => {
+			await Promise.all(
+				variables.accounts
+					.filter(account => account.authKey && post.original.addressReactions[account.account.address])
+					.map(account =>
+						BlockchainFeedApi.setReaction({
+							postId: post.msg.msgId,
+							reaction: null,
+							authKey: account.authKey,
+						}),
+					),
+			);
+
+			await reloadPost();
+		},
+		onError: error => {
+			toast(`Couldn't remove reaction 😟`);
+			console.error(error);
+		},
+	});
+
+	const removeReaction = async (accounts: DomainAccount[]) => {
+		if (setReactionMutation.isLoading || removeReactionMutation.isLoading) return;
+
+		await removeReactionMutation.mutateAsync({ accounts });
+	};
+
+	return (
+		<>
+			<SetReactionFlow onSelect={setReaction}>
+				{({ anchorRef, onAnchorClick }) => (
+					<button
+						ref={anchorRef}
+						className={reactionButtonClassName}
+						title="Reactions"
+						onClick={() => onAnchorClick()}
+					>
+						{setReactionMutation.isLoading || removeReactionMutation.isLoading ? (
+							<Spinner size={16} />
+						) : (
+							<SmileSvg />
+						)}
+					</button>
+				)}
+			</SetReactionFlow>
+
+			{reactionsCountsEntries.length ? (
+				<div className={css.reactions}>
+					{reactionsCountsEntries.map(([reaction, count]) => {
+						const accountsForReaction = getAccountsForReaction(reaction, post.original.addressReactions);
+
+						return accountsForReaction.length ? (
+							<ReactionBadge
+								reaction={reaction}
+								counter={count || 1}
+								isActive
+								onClick={() => removeReaction(accountsForReaction)}
+							/>
+						) : (
+							<SetReactionFlow key={reaction} initialReaction={reaction} onSelect={setReaction}>
+								{({ anchorRef, onAnchorClick }) => (
+									<ReactionBadge
+										ref={anchorRef}
+										reaction={reaction}
+										counter={count || 1}
+										onClick={onAnchorClick}
+									/>
+								)}
+							</SetReactionFlow>
+						);
+					})}
+				</div>
+			) : (
+				<div className={css.noReactions}>
+					{['❤️', '👍', '🎉'].map(reaction => (
+						<SetReactionFlow key={reaction} initialReaction={reaction} onSelect={setReaction}>
+							{({ anchorRef, onAnchorClick }) => (
+								<ReactionBadge ref={anchorRef} reaction={reaction} onClick={onAnchorClick} />
+							)}
+						</SetReactionFlow>
+					))}
+				</div>
+			)}
+		</>
+	);
+});
