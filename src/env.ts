ce920ce66ffe92cbfcf0e8c0c77574617756d8d8
Oleg Cherr <olegcherr@gmail.com>
2023-10-12 18:51:15 +0200

    Sync env/ci with Hub

--- src/env.ts
+++ src/env.ts
@@ -2,12 +2,7 @@
 
 export const NODE_ENV = process.env.NODE_ENV as string | undefined;
 
-export const REACT_APP__FEED_SERVER = process.env.REACT_APP__FEED_SERVER as string | undefined;
-export const REACT_APP__FEED_MANAGER = process.env.REACT_APP__FEED_MANAGER as string | undefined;
-export const REACT_APP__BLOCKCHAIN_FEED = process.env.REACT_APP__BLOCKCHAIN_FEED as string | undefined;
-export const REACT_APP__VAPID_PUBLIC_KEY = process.env.REACT_APP__VAPID_PUBLIC_KEY as string | undefined;
 export const REACT_APP__PUBLIC_URL = process.env.REACT_APP__PUBLIC_URL as string | undefined;
-export const REACT_APP__MV_PUBLIC_KEY = process.env.REACT_APP__MV_PUBLIC_KEY as string | undefined;
 
 export enum AppMode {
 	HUB = 'HUB',
@@ -16,6 +11,13 @@ export enum AppMode {
 }
 
 export const REACT_APP__APP_MODE = (process.env.REACT_APP__APP_MODE || AppMode.HUB) as AppMode;
+
+export const REACT_APP__FEED_SERVER = process.env.REACT_APP__FEED_SERVER as string | undefined;
+export const REACT_APP__FEED_MANAGER = process.env.REACT_APP__FEED_MANAGER as string | undefined;
+export const REACT_APP__BLOCKCHAIN_FEED = process.env.REACT_APP__BLOCKCHAIN_FEED as string | undefined;
+
+export const REACT_APP__VAPID_PUBLIC_KEY = process.env.REACT_APP__VAPID_PUBLIC_KEY as string | undefined;
+export const REACT_APP__MV_PUBLIC_KEY = process.env.REACT_APP__MV_PUBLIC_KEY as string | undefined;
 
 export enum OtcProvider {
 	AIRSWAP = 'AIRSWAP',
